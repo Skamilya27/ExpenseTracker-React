777baaa0a9d295e8771cc1aeaa9e0ffcbf6b6cd0
Suman <sumankamilya27@gmail.com>
2023-02-13 21:19:53 +0530

    learned to pass data

--- src/App.js
+++ src/App.js
@@ -1,27 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
-import ExpenseItem from './components/ExpenseItem';
+import logo from "./logo.svg";
+import "./App.css";
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+      store: "Online",
+    },
+    {
+      id: "e2",
+      title: "New TV",
+      amount: 799.49,
+      date: new Date(2021, 2, 12),
+      store: "Offline",
+    },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+      store: "Online",
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2021, 5, 12),
+      store: "Offline",
+    },
+  ];
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
-      <tr>
-        <td>Food</td>
-        <td>Rs</td>
-        <td>10</td>
-      </tr>
-      <tr>
-        <td>Petrol</td>
-        <td>Rs</td>
-        <td>100</td>
-      </tr>
-      <tr>
-        <td>Movies</td>
-        <td>Rs</td>
-        <td>200</td>
-      </tr>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+        store={expenses[0].store}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+        store={expenses[1].store}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+        store={expenses[2].store}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+        store={expenses[3].store}
+      ></ExpenseItem>
+
+      
+
+      
     </div>
   );
 }
